d7d8ffbbb7a0d7152d451796d8834e2ca6120f14
Eswaraprasadp <peswar1701@gmail.com>
2020-04-08 11:01:16 +0530

    Create download file on submit

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
+var a;
+var downloadDiv, downloadButton;
+const MIME_TYPE = 'text/plain';
+
+window.onload = () => {
+    a = document.createElement('a');
+    downloadDiv = document.getElementById('download-div');
+    downloadButton = document.getElementById('download-button');
+    window.URL = window.webkitURL || window.URL;
+}
+
 function insert(num){
             
-    document.form.getElementsById("load").values = num;  
+    // document.form.getElementsById("load").values = num;  
 }
-function submit()
-{
-    alert("jbjn!")
+
+function submit(){   
+    createDownloadFile();
+}
+
+function createDownloadFile() {
+    let bb = new Blob(['Hello World'], {type: MIME_TYPE});
+
+    a.download = 'output.txt';
+    a.href = window.URL.createObjectURL(bb);
+    a.dataset.downloadurl = [MIME_TYPE, a.download, a.href].join(':');
+    a.draggable = true; 
+    a.classList.add('dragout');
+
+    a.append(downloadButton);
+    downloadDiv.appendChild(a);
+}
+
+function download() {
+   
 }
